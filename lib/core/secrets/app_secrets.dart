8cce008b86fc1085324847e9c907f2d56671690a
Diksha <gr8bigdg@gmail.com>
2024-07-20 16:37:37 -0700

    Update app_secrets.dart

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,2 @@
 class AppSecrets {
-  static const supabaseUrl = "https://vldxcqbcmsjfqtedjyuu.supabase.co";
-  static const anonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsZHhjcWJjbXNqZnF0ZWRqeXV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0OTg5ODIsImV4cCI6MjAzNzA3NDk4Mn0.dNSxUFY9zVLdjf3lmwTLsAJkYKd8Si3LKnB3-i-amjI";
 }
